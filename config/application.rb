c180142d8dd384d0d053375f0239a49dff2d76ed
Francis Rangel <francis.rangel@gmail.com>
2014-11-27 14:08:01 -0200

    Adding more configuration and removing jquery from imports

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,21 @@ module AppReails
     config.neo4j.session_type = :server_db
     config.neo4j.session_path = ENV['GRAPHENEDB_URL'] || 'http://localhost:7474'
 
+    config.generators do |g|
+      g.helper false
+      g.view_specs false
+      g.assets false
+      g.integration_tool false
+    end
+
+    config.app_generators do |g|
+      g.orm :neo4j
+      g.test_framework :rspec
+    end
+
+    config.assets.initialize_on_precompile = false
+
+    config.middleware.use Rack::Deflater
 
   end
 end
